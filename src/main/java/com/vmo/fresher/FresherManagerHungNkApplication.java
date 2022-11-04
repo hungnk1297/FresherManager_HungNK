b4b02e87e825ef45e3f6faaa86f6c53e5240dae5
John Nguyen <hungnk@vmogroup.com>
2022-11-04 10:41:45 +0700

    First Commit

---
+++ src/main/java/com/vmo/fresher/FresherManagerHungNkApplication.java
@@ -0,0 +1,13 @@
+package com.vmo.fresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresherManagerHungNkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresherManagerHungNkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmo/fresher/FresherManagerHungNkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmo.fresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FresherManagerHungNkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
