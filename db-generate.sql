bc90be7f07442cbfa8ed8a735666be89eb0864aa
John Nguyen <hungnk1297@gmail.com>
2022-11-09 11:30:42 +0700

    Update | db-generate.sql | Update script to generate database

--- db-generate.sql
+++ db-generate.sql
@@ -1,1 +1,15 @@
 -- To generate database
+DROP TABLE IF EXISTS fresher;
+CREATE TABLE fresher
+(
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    deleted       BIT(1)       NOT NULL DEFAULT 0,
+    created_on    datetime     NULL,
+    last_modified datetime     NULL,
+    name          VARCHAR(255) NOT NULL,
+    dob           date         NOT NULL,
+    address       VARCHAR(255) NOT NULL,
+    phone         VARCHAR(255) NOT NULL,
+    email         VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_fresher PRIMARY KEY (id)
+);
